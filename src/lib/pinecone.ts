add95598f01a60e4d12122131a668ce40670e90c
Dhruv Sharma <dhruvamsh@gmail.com>
2024-06-06 10:20:59 +0530

    Update pinecone.ts

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -5,16 +5,9 @@ export const getPineconeClient = async () => {
 
   await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
     environment: "gcp-starter",
   });
-=======
->>>>>>> Stashed changes
-    environment: 'gcp-starter',
-  })
->>>>>>> a72fd59f1e987d6ba0560d82ba01e755955fe489
+
 
   return client;
 };
